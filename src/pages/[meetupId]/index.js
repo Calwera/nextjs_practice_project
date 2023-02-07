6ef656df962c74dcfc1f40b2b8733f48f4f55b36
Krzysztof Wróbel <calwera@gmail.com>
2023-02-07 22:43:10 +0100

    72

--- src/pages/[meetupId]/index.js
+++ src/pages/[meetupId]/index.js
@@ -11,4 +11,40 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/da/Frauenkirche_Munich_-_View_from_Peterskirche_Tower2.jpg",
+        id: "m1",
+        title: "first meetup",
+        address: "some street",
+        description: "laalal",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
